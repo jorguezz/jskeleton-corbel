c31e02752f179aa9ab58a8e50787889f6d2c5fac
danigar <daniel.garcia@bq.com>
2015-05-14 12:34:57 +0200

    Factory method
    Change-Id: If244017eeb278e1a7270b784863909bb1e4e5593

--- src/main.js
+++ src/main.js
@@ -24,131 +24,14 @@
     }
 
 })(this, function(root, corbel, JSkeleton, Backbone, q) {
-        'use strict';
 
-        /*globals*/
+    'use strict';
 
-        /* jshint unused: false */
+    /*globals*/
 
-        // JSkeleton.corbel = corbel;
-
-        var driver = JSkeleton.corbelDriver = corbel.getDriver();
-
-        JSkeleton.di.add('corbel', driver);
-
-        var _sync = Backbone.sync;
-
-        Backbone.sync = function(method, model, options) {
-
-            var requestParams = model.requestParams || {};
-            if (requestParams.resourceType || options.resourceType) {
-
-                var success = function(result) {
-                    options.success.call(this, result);
-                    return model;
-                };
-
-                var reject = function(result) {
-                    options.error.call(this, result);
-                    return q.reject(result || {
-                        error: 'invalid:operation'
-                    });
-                };
-
-                var fetchData = {
-
-                    resourceType: options.resourceType || requestParams.resourceType,
-                    data: options.data || requestParams.data,
-                    query: options.query || requestParams.query,
-                    queryDomain: options.queryDomain || requestParams.queryDomain,
-                    search: options.search || requestParams.search,
-                    page: options.page || requestParams.page,
-                    sort: options.sort || requestParams.sort,
-                    aggregation: options.aggregation || requestParams.aggregation
-                };
-
-                // deprecated notice
-                if (options.rel || requestParams.rel) {
-                    console.warn('deprecated: rel param');
-                }
-                if (options.uri || requestParams.uri) {
-                    console.warn('deprecated: uri param');
-                }
-
-                var builder,
-                    isCollectionResponse = model instanceof Backbone.Collection,
-                    promise;
-
-                var id;
-
-                //----------IT IS NECESSARY 'RESOURCES' ----------
-                if (isCollectionResponse || method === 'create') {
-                    builder = resources.collection(fetchData.resourceType);
-                } else {
-                    id = options.id || model.get('id');
-                    builder = resources.resource(fetchData.resourceType, id);
-                }
-
-                switch (method) {
-                    case 'read':
-                        if (isCollectionResponse) {
-                            promise = builder.get(fetchData).then(function(data) {
-                                return success(data);
-                            });
-                        } else {
-                            promise = builder.get();
-
-                            promise = promise.then(function(data) {
-                                return success(data);
-                            });
-                        }
-                        return promise.fail(function(error) {
-                            return reject(error);
-                        });
-                    case 'create':
-                        if (!isCollectionResponse) {
-                            promise = builder.add(model.toJSON());
-                            return promise.then(function(id) {
-                                return success({
-                                    id: id
-                                });
-                            }).fail(function(error) {
-                                return reject(error);
-                            });
-                        }
-                        return reject();
-                    case 'update':
-                        if (!isCollectionResponse) {
-                            return builder.update(model.toJSON()).then(function() {
-                                return success(model.toJSON());
-                            }).fail(function(error) {
-                                return reject(error);
-                            });
-                        }
-                        // @todo: a model fetched from relation with matrix uri should remove
-                        // relation params in order to update like a normal model
-                        return reject();
-                    case 'patch':
-                        // Not supported
-                        return reject();
-                    case 'delete':
-                        if (!isCollectionResponse) {
-                            return builder.delete().then(function() {
-                                return success(model);
-                            }).fail(function(error) {
-                                return reject(error);
-                            });
-                        }
-                        return reject();
-                    default:
-                        return reject();
-                }
-
-            } else {
-                return _sync(method, model, options);
-            }
+    /* jshint unused: false */
 
 
-            return JSkeleton.corbel;
+    return JSkeleton.corbel;
 
-        });
+});
