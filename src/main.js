a9fdf8c081361d07d4dd26ac491936797bc5b6a0
danigar <daniel.garcia@bq.com>
2015-05-14 17:32:28 +0200

    Remove q dependency
    Change-Id: I1cc9455bd204cf352a9e622422ecc5f37feacf7e

--- src/main.js
+++ src/main.js
@@ -6,24 +6,22 @@
     if (typeof define === 'function' && define.amd) {
         define(['jskeleton',
             'corbel-js',
-            'backbone',
-            'q'
-        ], function(JSkeleton, corbel, Backbone, q) {
+            'backbone'
+        ], function(JSkeleton, corbel, Backbone) {
             return factory.call(root, root, corbel, JSkeleton, Backbone, q);
         });
     } else if (typeof module !== 'undefined' && module.exports) {
         var corbel = require('corbel-js'),
             Backbone = require('backbone'),
-            q = require('q'),
             JSkeleton = require('jskeleton');
 
-        module.exports = factory(root, corbel, JSkeleton, Backbone, q);
+        module.exports = factory(root, corbel, JSkeleton, Backbone);
 
     } else if (root !== undefined) {
-        factory.call(root, root, root.corbel, root.Jskeleton, root.Backbone, root.q);
+        factory.call(root, root, root.corbel, root.Jskeleton, root.Backbone);
     }
 
-})(this, function(root, corbel, JSkeleton, Backbone, q) {
+})(this, function(root, corbel, JSkeleton, Backbone) {
 
     'use strict';
 
